7d33a5239c9ece614b8d2960bbdf89c8cddd11e5
jeongjw <projaewoo@gmail.com>
2022-07-20 11:36:46 +0900

    react-three-fiber
    1. object에 material 적용

--- src/contents/components/reactThreeFiber/reactThreeFiber.tsx
+++ src/contents/components/reactThreeFiber/reactThreeFiber.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as THREE from 'three';
-import {Canvas, useLoader} from "@react-three/fiber";
+import {Canvas, useGraph, useLoader} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 import styled from "styled-components";
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
@@ -8,6 +8,7 @@ import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
 {/*
     TODO.
         1. loading progress
+            https://docs.pmnd.rs/react-three-fiber/API/hooks#use-graph
 */
 }
 
@@ -25,10 +26,15 @@ const ReactThreeFiber = () => {
     ])
 
     const Model = (objectUrl: string) => {
-        const obj = useLoader(OBJLoader, objectUrl);
-        return (
-          <primitive object={obj}/>
-        );
+        const scene = useLoader(OBJLoader, objectUrl);
+        const { nodes, materials } = useGraph(scene);
+        materials[Object.keys(materials)[0]].displacementMap = roughnessMap;
+        materials[Object.keys(materials)[0]].displacementScale = 0.1;
+        materials[Object.keys(materials)[0]].map = map;
+        materials[Object.keys(materials)[0]].normalMap = normalMap;
+        materials[Object.keys(materials)[0]].aoMap = aoMap;
+
+        return <mesh geometry={nodes[Object.keys(nodes)[0]].geometry} material={materials[Object.keys(materials)[0]]}/>
     }
 
     return (
@@ -38,8 +44,6 @@ const ReactThreeFiber = () => {
               <pointLight position={[10, 15, 10]} angle={0.3}/>
               <mesh>
                   {Model(objectSrc)}
-                  {/*<sphereBufferGeometry attach={'geometry'} args={[2, 64, 64]}/>*/}
-                  <meshStandardMaterial attach={'material'} map={map} aoMap={aoMap} normalMap={normalMap} roughnessMap={roughnessMap}/>
               </mesh>
           </Canvas>
       </Wrapper>
