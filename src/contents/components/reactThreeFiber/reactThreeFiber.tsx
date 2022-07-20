23b65b0c6f757318a372397da60d5dbdecaf1519
jeongjw <projaewoo@gmail.com>
2022-07-20 10:39:32 +0900

    react-three-fiber
    1. obj loader 적용

--- src/contents/components/reactThreeFiber/reactThreeFiber.tsx
+++ src/contents/components/reactThreeFiber/reactThreeFiber.tsx
@@ -1,16 +1,21 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import * as THREE from 'three';
 import {Canvas, useLoader} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 import styled from "styled-components";
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
+
+{/*
+    TODO.
+        1. loading progress
+*/
+}
 
 const Wrapper = styled.div`
     height: 100vh
 `
 
 const ReactThreeFiber = () => {
-
     const objectSrc = `${process.env.PUBLIC_URL}/NT_NO061/NT_NO061.obj`
     const [aoMap, map, normalMap, roughnessMap] = useLoader(THREE.TextureLoader, [
         `${process.env.PUBLIC_URL}/NT_NO061/Textures/T_NT_NO061_AO.png`,
@@ -19,21 +24,21 @@ const ReactThreeFiber = () => {
         `${process.env.PUBLIC_URL}/NT_NO061/Textures/T_NT_NO061_R.png`
     ])
 
-    const Model = (url: string) => {
-        const obj = useLoader(OBJLoader, url);
-        return <primitive object={obj}/>
+    const Model = (objectUrl: string) => {
+        const obj = useLoader(OBJLoader, objectUrl);
+        return (
+          <primitive object={obj}/>
+        );
     }
 
     return (
       <Wrapper>
           <Canvas onCreated={state => state.gl.setClearColor('black')}>
               <OrbitControls/>
-              {/*<Stars/>*/}
-              <ambientLight intensity={0.5}/>
               <pointLight position={[10, 15, 10]} angle={0.3}/>
               <mesh>
-                {/*{Model(objectSrc)}*/}
-                  <sphereBufferGeometry attach={'geometry'} args={[2, 64, 64]}/>
+                  {Model(objectSrc)}
+                  {/*<sphereBufferGeometry attach={'geometry'} args={[2, 64, 64]}/>*/}
                   <meshStandardMaterial attach={'material'} map={map} aoMap={aoMap} normalMap={normalMap} roughnessMap={roughnessMap}/>
               </mesh>
           </Canvas>
