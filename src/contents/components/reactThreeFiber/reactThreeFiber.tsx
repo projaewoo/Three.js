c6cc915d3e3e3205a4e8dfc6a5bf642147dee36d
jeongjw <projaewoo@gmail.com>
2022-07-26 18:58:49 +0900

    react three fiber
    1. video 기능 추가
    2. tsconfig에서 null 값 엄격하지 않게 체크하도록 환경설정 변경

--- src/contents/components/reactThreeFiber/reactThreeFiber.tsx
+++ src/contents/components/reactThreeFiber/reactThreeFiber.tsx
@@ -1,6 +1,6 @@
 import React, {useRef} from 'react';
 import * as THREE from 'three';
-import {Canvas, useGraph, useLoader} from "@react-three/fiber";
+import {Canvas, useFrame, useGraph, useLoader} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 import styled from "styled-components";
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
@@ -13,6 +13,8 @@ import LightController from "./lightController";
             https://docs.pmnd.rs/react-three-fiber/API/hooks#use-graph
 */
 }
+
+type moveType = 'rightToLeft' | 'leftToRight' | 'bottomToTop' | 'topToBottom' | 'rotation'
 
 const Wrapper = styled.div`
     height: 100vh
@@ -38,15 +40,68 @@ const ReactThreeFiber = () => {
         metalness: 0.80           // 좀 더 메탈스럽게 (색을 더 선명하게)
     })
     
-    const playVideo = () => {
-        cameraControls.current?.reset(true);
-        setTimeout(() => {
-            cameraControls.current?.rotate(Math.PI, 0, true)
-        }, 1000)
-        setTimeout(() => {
-            cameraControls.current?.rotate(Math.PI, 0, true)
-        }, 2000)
+    const [startVideo, setStartVideo] = React.useState<boolean>(false);
+    const [type, setType] = React.useState<moveType>('leftToRight')
+
+    let startPoint = 3
+
+    function Thing() {
+        const lookAtPos = new THREE.Vector3()
+        let height = 0.5;
+
+        useFrame(state => {
+            switch (type as moveType) {
+                case 'leftToRight': {
+                    console.log('leftToRight')
+                    lookAtPos.x = -Math.sin(startPoint * 0.5)
+                    lookAtPos.y = height;
+                    lookAtPos.z = Math.sin(startPoint * 0.5)
+                    startPoint += 0.01
+
+                    if (lookAtPos.x > 0.99) {
+                        setType('bottomToTop')
+                    }
+                    state.camera.lookAt(lookAtPos)
+                    state.camera.updateProjectionMatrix()
+                    break;
+                }
+                case 'bottomToTop': {
+                    console.log('bottomToTop')
+                    lookAtPos.x = 0
+                    lookAtPos.y = Math.sin(-startPoint * 0.5)
+                    lookAtPos.z = 0
+                    startPoint += 0.01
+
+                    if (lookAtPos.y > 0.99) {
+                        setType('rightToLeft')
+                    }
+                    state.camera.lookAt(lookAtPos)
+                    state.camera.updateProjectionMatrix()
+                    break;
+                }
+                case 'rightToLeft': {
+                    console.log('rightToLeft')
+                    lookAtPos.x = Math.sin(startPoint * 0.5)
+                    lookAtPos.y = height;
+                    lookAtPos.z = -Math.sin(startPoint * 0.5)
+                    startPoint += 0.01
+
+                    if (lookAtPos.z > 0.99) {
+                        setType('leftToRight')
+                    }
+                    state.camera.lookAt(lookAtPos)
+                    state.camera.updateProjectionMatrix()
+                    break;
+                }
+                default: return null;
+            }
+        })
+
+        return (
+          <mesh/>
+        )
     }
+
 
     return (
       <Wrapper>
@@ -57,21 +112,30 @@ const ReactThreeFiber = () => {
               <CameraControls ref={cameraControls} />
               <LightController/>
               <OrbitControls/>
+              <axesHelper args={[5, 5, 5]}/>
               <mesh
                 position={[0, -2, 0]}
                 geometry={geometry}
                 material={material}
               />
+              {startVideo && <Thing/>}
           </Canvas>
           <div style={{ position: 'absolute', top: '0' }}>
               <button type={'button'} onClick={() => cameraControls.current?.rotate(Math.PI / 4, 0, true)}>
                   rotate theta 45deg
               </button>
-              <button type={'button'} onClick={() => cameraControls.current?.reset(true)}>
+              <button
+                type={'button'}
+                onClick={() => {
+                    cameraControls.current?.reset(true);
+                    setStartVideo(false);
+                    setType('leftToRight')
+                }}
+              >
                   reset
               </button>
-              <button type={'button'} onClick={playVideo}>
-                  play video
+              <button type={'button'} onClick={() => setStartVideo(true)}>
+                  start video
               </button>
           </div>
       </Wrapper>
